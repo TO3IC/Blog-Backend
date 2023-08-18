beaaf6fb528bea192a0081e1f0eac886a0e6590a
TOXIC <108856741+TO3IC@users.noreply.github.com>
2023-08-18 21:29:13 +0800

    fixed backend

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import { config } from "dotenv";
 import express, { json, urlencoded } from 'express';
+import client from "./connection.js";
 import bodyParser from "body-parser";
 import cors from 'cors';
 import { MongoClient, ServerApiVersion } from "mongodb";
+import router from "./routes/blog.js";
 
 config();
 
@@ -13,53 +15,22 @@ const corsOptions = {
     origin: allowedOrigins,
 };
 
+const port = process.env.PORT || 4500
+const uri = process.env.APIKEY
 const app = express();
+
 app.use(cors(corsOptions));
 app.use(json());
 app.use(urlencoded({ limit: "500kb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "500kb", extended: false }));
 
-const port = process.env.PORT || 4500
 
-const uri = process.env.APIKEY
-
-const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
-})
-
-
-app.get('/status', (req, res) => {
-    res.send({ message: "Up and running" })
-});
+app.use("/api/blogs",router)
 
 app.get('/', async (req, res) => {
-    try {
-        await client.connect();
-        const database = client.db("test");
-        const collection = database.collection("blogs");
-
-        const query = {};
-        const cursor = collection.find(query);
-
-        const result = await cursor.toArray();
-        if (result.length < 0) {
-            res.status(404).send({ error: "Blogs not found" })
-        } else {
-            res.send(result)
-        }
-    } catch (error) {
-        console.log("error");
-    } finally {
-        await client.close();
-    }
+    res.end("Use /api/blogs")
 })
 
-
-
 app.listen(port, () => {
-    console.log(`[server]: Hello Toxic I'm your fucking server running on http://localhost:${port}`)
+    console.log(`[server]: Hello, I'm your server running on http://localhost:${port}`)
 })
